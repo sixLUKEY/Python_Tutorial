9e3f0188369b6fa78d49937fffb9e80de61bb716
Luke Evertson <130046695+sixLUKEY@users.noreply.github.com>
2023-12-10 09:27:50 +0200

    type section complete

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ print('hello world')
 
 if 5 > 2:
     print('You are correct!')
+
+x = 5
+y = 'hello world`'
+
+#this is a comment
+print( x )
+
+x = str( 3 )
+y = int( 3 )
+z = float( 3 )
+
+print(type( x ))
+print(type( y ))
